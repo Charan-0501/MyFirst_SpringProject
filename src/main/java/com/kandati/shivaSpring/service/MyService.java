b03c929c551652b88b061ad63f23d8466cf98324
Shiva Charan Reddy Kandati <126412477+Charan-0501@users.noreply.github.com>
2025-01-27 23:19:57 +0530

    Updated MyService.java
    Instantiated Arraylist inside constructor in MyService.java and declaration outside of the constructor.

--- src/main/java/com/kandati/shivaSpring/service/MyService.java
+++ src/main/java/com/kandati/shivaSpring/service/MyService.java
@@ -6,11 +6,11 @@ import java.util.ArrayList;
 
 public class MyService {
 
-    ArrayList<CourseCreation>  course_list=new ArrayList<>();;
+    ArrayList<CourseCreation>  course_list;
 
     public MyService() {
 
-       // course_list=new ArrayList<>();
+        course_list=new ArrayList<>();
         course_list.add(new CourseCreation(101,"Java SpringBoot","3 Months",4500.00,true));
         course_list.add(new CourseCreation(102,"Python Django","2 Months",5000.00,true));
         course_list.add(new CourseCreation(103,"JavaScript Fundamentals","1 Months",1500.00,true));
